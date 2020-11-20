5d48924bdaeb36d515c5a192056e6340394c3c55
Vladislav <v.butirsky@gmail.com>
2020-11-20 15:57:03 +0700

    After 2 review

--- homework.py
+++ homework.py
@@ -12,7 +12,6 @@ class Record:
 
 
 class Calculator:
-    TODAY = dt.date.today()
 
     def __init__(self, limit):
         self.limit = limit
@@ -22,16 +21,15 @@ class Calculator:
         self.records.append(new_record)
 
     def get_today_stats(self):
+        today = dt.date.today()
         return sum(records.amount for records in self.records
-                   if records.date == self.TODAY)
+                   if records.date == today)
 
     def get_week_stats(self):
-        week = 0
-        week_back = self.TODAY - dt.timedelta(7)
-        for record in self.records:
-            if self.TODAY >= record.date > week_back:
-                week += record.amount
-        return week
+        today = dt.date.today()
+        week_back = today - dt.timedelta(7)
+        return sum(record.amount for record in self.records
+                   if today >= record.date > week_back)
 
     def __str__(self):
         return '\n'.join(self.records)
@@ -51,12 +49,13 @@ class CashCalculator(Calculator):
     }
 
     def get_today_cash_remained(self, currency):
-        if self.get_today_remained() == 0:
+        left = self.get_today_remained()
+        if left == 0:
             return "Денег нет, держись"
         if currency not in self.currencies:
             return "Валюта не поддерживается"
         currency_name, currency_rate = self.currencies[currency]
-        remained = (self.get_today_remained() / currency_rate)
+        remained = (left / currency_rate)
         cash_remained = round(remained, 2)
         cash_remained_abs = abs(cash_remained)
         if cash_remained > 0:
@@ -67,7 +66,8 @@ class CashCalculator(Calculator):
 
 class CaloriesCalculator(Calculator):
     def get_calories_remained(self):
-        if self.get_today_remained() > 0:
+        left = self.get_today_remained()
+        if left > 0:
             return (f"Сегодня можно съесть что-нибудь ещё, но с общей "
-                    f"калорийностью не более {self.get_today_remained()} кКал")
+                    f"калорийностью не более {left} кКал")
         return "Хватит есть!"
